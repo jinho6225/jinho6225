3e2f472b9259fcac842aa3b766c441f217ebcaee
jinho6225 <jinho6225@hotmail.com>
2020-02-26 01:13:14 -0800

    initialized with react package.json webpack with babel

---
+++ src/components/List.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const List = (props) => {
+    return (
+      <div>
+        <h3>please check links below</h3>
+        <ul>
+          <li><a href="http://memory-match.jinho6225.com/">Memory-match</a></li>
+          <li><a href="http://vanillaJS.jinho6225.com/">TodoList-Clock-Weather</a></li>
+          <li><a href="http://webapp.jinho6225.com/">Simple webapp</a></li>
+          <li><a href="http://gradetable.jinho6225.com/">Student-Grade-Table</a></li>
+          <li><a href="http://pororo-memory-match.jinho6225.com/">PoRoRo-Memory-match</a></li>
+          <li><a href="http://jinho6225.github.io/">Jinho's blog</a></li>
+        </ul>
+      </div>
+    )
+}
+
+export default List;
