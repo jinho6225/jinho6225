9a02aea2bde9ddb7dc7ce578f1486af6af6d6e25
jinho6225 <jinho6225@hotmail.com>
2020-02-26 01:35:50 -0800

    updated#2

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,9 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: path.resolve(__dirname, './src'),
+  entry: path.resolve(__dirname, '/src'),
   output: {
-    path: path.resolve(__dirname, './dist'),
+    path: path.resolve(__dirname, '/dist'),
     filename: 'bundle.js',
   },
   resolve: {
